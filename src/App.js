a66901af5811a1f366093fc2facde4e231d3dca0
odayabueid <odayabueid1992m@gmail.com>
2019-05-27 18:14:09 +0300

    Rating and Accept componnent

--- src/App.js
+++ src/App.js
@@ -1,61 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Accept from './components/AcceptComponent.js'
+import Foo from './components/Rating.js'
+/////////////////////////////////
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      greeting: ''
+      mechinfo:{ 
+         userName:"odayabueid",
+         phoneNum:"78090332",
+         mechImg:"https://thumbs.dreamstime.com/z/mechanical-engineer-29186038.jpg"
+      }
     };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    
   }
+    render(){
+      return(
+        <div>
+          <Accept
+             mechinfo={this.state.mechinfo}
+          />
+          <Foo />
+        </div>
+        )
+    }
 
-  handleChange(event) {
-    this.setState({ name: event.target.value });
   }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    fetch(`/home?name=${encodeURIComponent(this.state.name)}`)
-      .then(response => response.json())
-      .then(state => this.setState(state));
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="name">Enter your name: </label>
-            <input
-              id="name"
-              type="text"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-            <button type="submit">Click me</button>
-          </form>
-          <p>{this.state.greeting}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-
-}
-
-
 export default App;
